0e6a4d94d2c27ab568abca130ee7a17c27890d50
ibrahimcansun <60621945+ibrahimcansun@users.noreply.github.com>
2021-04-09 01:45:05 +0300

    Add files via upload

---
+++ basit/Soru1/Soru1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soru1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Text = "0";
+            comboBox1.Items.Add("20");
+            comboBox1.Items.Add("30");
+            comboBox1.Items.Add("40");
+            comboBox1.Items.Add("50");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hesapla hesap = new Hesapla();
+            hesap.vizeYuzde = int.Parse(comboBox1.Text);
+            hesap.vize = int.Parse(textBox1.Text);
+            hesap.final = int.Parse(textBox2.Text);
+
+            textBox3.Text = hesap.BasariAl().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ basit/Soru1/Soru1/Hesapla.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru1
+{
+    class Hesapla
+    {
+        public Single vize = 0;
+        public Single final = 0;
+        public Single vizeYuzde = 0;
+        public Single sonuc;
+
+        public Single BasariAl()
+        {
+            sonuc = ((vize * vizeYuzde) / 100) + ((final * (100 - vizeYuzde)) / 100);
+            return sonuc;
+        }
+    }
+}
